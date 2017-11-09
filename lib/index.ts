c5f3326926174bf5a73aa8522601e55924de23dd
Michael Charkin <michael@lablablab.co>
2017-11-08 17:11:52 -0800

    first version

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,39 @@
 
- 
-export function test(str: string): string {
-  return str
+export class ObjPath {
+
+  static valuesAtPath(path:string[], obj:any): any[] {
+    const acc: any[] = []
+    this._pathHelper(acc, path, 0, obj)
+    return acc;
+  }
+
+  private static _pathHelper(accumulator:any[], path: string[], idx:number, subObj:any) {
+    // current sub object is null, we are done
+    if(subObj == null) {
+      return;
+    }
+
+    if(path == null) {
+      return;
+    }
+
+    // index is past the path, take the current object
+    if(idx >= path.length) {
+      accumulator.push(subObj)
+      return;
+    }
+
+    if(Array.isArray(subObj)) {
+      // current object is an array, apply serach for values to each value in array
+      subObj.forEach( v => {
+        this._pathHelper(accumulator, path, idx, v)
+      })
+    } else {
+      // access the object at an index
+      const pathKey = path[idx];
+      const newSubObj = subObj[pathKey]
+      this._pathHelper(accumulator, path, idx + 1, newSubObj)
+    }
+  }
+
 }
--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -1,10 +1,117 @@
-import {test} from "../lib"
+import {ObjPath} from "../lib"
 import {} from "jest"
 
-describe("sample test", () => {
+describe("obj path", () => {
 
-  it("should do something", () => {
-    expect( test("foo") ).toEqual("foo")
+  const testObj = { 
+    key1: "value1",
+    key2: "value2",
+    key3: {
+      sKey1: "sValue1",
+      sKey2: {
+        ssKey1: "ssValue1",
+        ssKey2: "ssValue2",
+        ssKey3: "ssValue3"
+      }
+    }
+  }
+
+  const testWithArrayObj = { 
+    key1: "value1",
+    key2: "value2",
+    enrollment: {
+      sKey2: {
+        ssKey1: "ssValue1",
+        ssKey2: "ssValue2",
+        ssKey3: "ssValue3"
+      },
+      wizards:[
+        { 
+          name: "bob", 
+          abilities: ["sleep", "eat", "snore"],
+          location: {
+            city: "boston",
+            street:" boylston street"
+          }
+        },
+        { 
+          name: "george",
+          abilities: ["eat", "magic", "pranks" ],
+          location: {
+            city: "london",
+            street: "shack"
+          }
+        },
+        { 
+          name: "harry",
+          abilities: [ "eat", "magic" ],
+          location: {
+            city: "london",
+            street: "privet drive"
+          }
+        },
+      ]
+    }
+  }
+
+  it("should succeed on an empty object", () => {
+    const obj = {}
+    const path = ["foo", "fi", "fee"]
+    expect( ObjPath.valuesAtPath(path, obj) ).toEqual([])
+  })
+
+  it("should succeed on an empty path", () => {
+    const obj = { key: "value" }
+    const path:string[] = []
+    expect( ObjPath.valuesAtPath(path, obj) ).toEqual([obj])
+  })
+
+  it("should succeed on an null object", () => {
+    const obj = null
+    const path:string[] = []
+    expect( ObjPath.valuesAtPath(path, obj) ).toEqual([])
+  })
+
+  it("should succeed on a null path", () => {
+    const obj = { key: "value" }
+    expect( ObjPath.valuesAtPath(<any>null, obj) ).toEqual([])
+  })
+
+
+  it("should extract a value when the path is too long", () => {
+    const obj = { 
+      key1: "value1",
+      key2: "value2",
+      key3: {
+        subKey1: "subValue1",
+        subKey2: "subValue2"
+      }
+    }
+    const path = ["key3", "subKey1", "subSubKey2"]
+    expect( ObjPath.valuesAtPath(path, obj) ).toEqual([])
+  })
+
+  it("should extract a value when the path is short", () => {
+    const path = ["key3", "sKey2"]
+    const expected = {
+      ssKey1: "ssValue1",
+      ssKey2: "ssValue2",
+      ssKey3: "ssValue3"
+    }
+
+    expect( ObjPath.valuesAtPath(path, testObj) ).toEqual([expected])
+  })
+
+  it("should extract a value when the object has an array", () => {
+    const path = ["enrollment", "wizards", "name"]
+    const expected = ["bob", "george", "harry"]
+    expect( ObjPath.valuesAtPath(path, testWithArrayObj) ).toEqual(expected)
+  })
+
+  it("should extract a deep value when the object has an array", () => {
+    const path = ["enrollment", "wizards", "location", "city"]
+    const expected = ["boston", "london", "london"]
+    expect( ObjPath.valuesAtPath(path, testWithArrayObj) ).toEqual(expected)
   })
 
 })
